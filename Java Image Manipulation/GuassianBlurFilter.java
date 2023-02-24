97c3cb2bf18f66790c7765bd8493738de4548f63
Electro-Corp <chinmaytanmay@outlook.com>
2023-02-23 18:03:50 -0700

    fix gaussian blur

--- Java Image Manipulation/GuassianBlurFilter.java
+++ Java Image Manipulation/GuassianBlurFilter.java
@@ -9,7 +9,81 @@ public class GuassianBlurFilter implements Filter{
                 Pixel g = data[row][col];
                 int avgR  = 0, avgG = 0, avgB = 0;
                 int val = 4;
-                for(int i = 0; i < val; i++){
+                int[][] weights = {
+                	{1,2,1},
+                	{2,4,2},
+                	{1,2,1}
+                };
+                // X (+ and -)
+                int weight = 4,rR = 0, gG = 0, bB = 0;
+                for(int xR = 0; xR < 6; xR++){
+                	
+                	if(xR < 3){
+                		
+                		try{
+				        rR += old[row][col+xR].getRed();
+				        bB += old[row][col+xR].getBlue();
+				        gG += old[row][col+xR].getGreen();
+				        
+                    		}catch(Exception e){
+				        rR += old[row][col].getRed();
+				        bB += old[row][col].getBlue();
+				        gG += old[row][col].getGreen();
+                    		}
+                    		weight = weights[0][xR];
+                	}else{
+                		try{
+				        rR += old[row+val-1][col-xR-3].getRed();
+				        bB += old[row+val-1][col-xR-3].getBlue();
+				        gG += old[row+val-1][col-xR-3].getGreen();
+                    		}catch(Exception e){
+				        rR += old[row][col].getRed();
+				        bB += old[row][col].getBlue();
+				        gG += old[row][col].getGreen();
+                    		}
+                    		weight = weights[0][xR-3];
+                	}
+                	avgR += rR * weight;
+                	avgG += gG * weight;
+                	avgB += bB * weight;
+                	avgR /= val*val;
+                	avgG /= val*val;
+               	 	avgB /= val*val;
+                }
+                for(int xR = 0; xR < 6; xR++){
+                	if(xR < 3){
+                		
+                		try{
+				        rR += old[row-xR][col-val-1].getRed();
+				        bB += old[row-xR][col-val-1].getBlue();
+				        gG += old[row-xR][col-val-1].getGreen();
+				        
+                    		}catch(Exception e){
+				        rR += old[row][col].getRed();
+				        bB += old[row][col].getBlue();
+				        gG += old[row][col].getGreen();
+                    		}
+                    		weight = weights[0][xR];
+                	}else{
+                		try{
+				        rR +=  old[row-xR-3][col-val-1].getRed();
+				        bB +=  old[row-xR-3][col-val-1].getBlue();
+				        gG +=  old[row-xR-3][col-val-1].getGreen();
+                    		}catch(Exception e){
+				        rR += old[row][col].getRed();
+				        bB += old[row][col].getBlue();
+				        gG += old[row][col].getGreen();
+                    		}
+                    		weight = weights[0][xR-3];
+                	}
+                	avgR += rR * weight;
+                	avgG += gG * weight;
+                	avgB += bB * weight;
+                	avgR /= val*val;
+		        avgG /= val*val;
+		        avgB /= val*val;
+                }
+                /*for(int i = 0; i < val; i++){
                     try{
                         avgR += old[row][col+i].getRed();
                         avgB += old[row][col+i].getBlue();
@@ -19,8 +93,8 @@ public class GuassianBlurFilter implements Filter{
                         avgB += old[row][col].getBlue();
                         avgG += old[row][col].getGreen();
                     }
-                }
-                for(int i = 0; i < val; i++){
+                }*/
+                /*for(int i = 0; i < val; i++){
                     try{
                         avgR += old[row+i][col+val-1].getRed();
                         avgB += old[row+i][col+val-1].getBlue();
@@ -30,8 +104,8 @@ public class GuassianBlurFilter implements Filter{
                         avgB += old[row][col].getBlue();
                         avgG += old[row][col].getGreen();
                     }
-                }
-                for(int i = val; i > 0; i--){
+                }*/
+               /* for(int i = val; i > 0; i--){
                     try{
                         avgR += old[row+val-1][col-i].getRed();
                         avgB += old[row+val-1][col-i].getBlue();
@@ -42,6 +116,8 @@ public class GuassianBlurFilter implements Filter{
                         avgG += old[row][col].getGreen();
                     }
                 }
+                */
+                /*
                 for(int i = val; i > 0; i--){
                     try{
                         avgR += old[row-i][col-val-1].getRed();
@@ -53,9 +129,8 @@ public class GuassianBlurFilter implements Filter{
                         avgG += old[row][col].getGreen();
                     }
                 }
-                avgR /= val*val;
-                avgG /= val*val;
-                avgB /= val*val;
+                */
+                
                 data[row][col].setBlue(avgB);
                 data[row][col].setGreen(avgG);
                 data[row][col].setRed(avgR);
